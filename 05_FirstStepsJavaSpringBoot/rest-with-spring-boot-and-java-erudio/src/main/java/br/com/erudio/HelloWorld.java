047e1691a74373d3ce375adc86396b968faec46d
Guilherme <guilhermevmonteiro2512@gmail.com>
2023-10-11 15:53:39 -0300

    First Steps in Java with Spring Boot

---
+++ 05_FirstStepsJavaSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/HelloWorld.java
@@ -0,0 +1,23 @@
+package br.com.erudio;
+
+public class HelloWorld {
+
+	private final long id;
+	private final String content;
+	
+	public HelloWorld(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+
+}
